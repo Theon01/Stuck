5c84e662149a084d2228d6d80bc84d71424b1182
Haitam Rifi <hamren01@gmail.com>
2012-12-06 03:41:51 +0000

    Revert "Revert "cb classes populated""
    This reverts commit 21f06fb3c903777f85074c1b0d77bfda09de45f7.

--- scmtool/src/main/java/ma/aui/scmtool/visitor/AstExplorerVisitor.java
+++ scmtool/src/main/java/ma/aui/scmtool/visitor/AstExplorerVisitor.java
@@ -1,21 +1,41 @@
 package ma.aui.scmtool.visitor;
+import java.util.Vector;
+
+import ma.aui.scmtool.model.CuMetadata;
 
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 public class AstExplorerVisitor extends ASTVisitor {
+	
+	CuMetadata cuMetadata;
+	
+	public AstExplorerVisitor() {
+		super();
+		cuMetadata = new CuMetadata(new Vector<String>());
+	}
 
 	@Override
 	public boolean visit(TypeDeclaration node) {
 		
 		if (! node.isInterface()){
 			
-			
-			System.out.println(node.getName());
+			cuMetadata.getTypesNames().add(node.getName().toString());
+			System.out.println(cuMetadata.getTypesNames().toString());
 		}
 		
 		return true;
 	}
+
+	public CuMetadata getCuMetadata() {
+		return cuMetadata;
+	}
+
+	public void setCuMetadata(CuMetadata cuMetadata) {
+		this.cuMetadata = cuMetadata;
+	}
+	
+	
 	
 	
 
