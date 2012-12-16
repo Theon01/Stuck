e2d82110e9ef7aad56e5088915ec9978ef052693
Saad Taame <s.taame@aui.ma>
2012-12-16 20:16:34 +0000

    Update scmtool/src/main/java/ma/aui/scmtool/visitor/AstExplorerVisitor.java
    Source code rewrite (indentation)

--- scmtool/src/main/java/ma/aui/scmtool/visitor/AstExplorerVisitor.java
+++ scmtool/src/main/java/ma/aui/scmtool/visitor/AstExplorerVisitor.java
@@ -34,9 +34,12 @@ import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.WhileStatement;
 import org.eclipse.jface.text.projection.Fragment;
 
-public class AstExplorerVisitor extends ASTVisitor {
-
-	//CuMetadata cuMetadata;
+public class AstExplorerVisitor extends ASTVisitor
+{
+	private Stack stStack;
+	private Stack methodsStack;
+	private Stack classesStack;
+	private Stack cuStack;
 	
 	Vector<Statement> statementsList;
 	Vector<Method> methodsList;
@@ -44,224 +47,243 @@ public class AstExplorerVisitor extends ASTVisitor {
 	Vector<CompilationUnit> cunitsList;
 	
 	Integer nblevelsCount = 0;
-	
 	Integer nbOfPublicMembers = 0;
+	
+	/**
+	 * Constructor
+	 */
+	public AstExplorerVisitor() 
+	{
+		super();
+		
+		stStack      = new Stack<ASTNode>();
+		methodsStack = new Stack<ASTNode>();
+		classesStack = new Stack<ASTNode>();
+		cuStack      = new Stack<ASTNode>();
+		
+		statementsList =  new Vector<Statement>();
+		methodsList    = new Vector<Method>();
+		classesList    = new Vector<Class>();
+		cunitsList     = new Vector<CompilationUnit>();
+	}
 
-	public Vector<CompilationUnit> getCunitsList() {
+	/**
+	 * Getters and setters
+	 */ 
+	public Vector<CompilationUnit> getCunitsList()
+	{
 		return cunitsList;
 	}
-
-
-
-	public void setCunitsList(Vector<CompilationUnit> cunitsList) {
+	
+	public void setCunitsList(Vector<CompilationUnit> cunitsList)
+	{
 		this.cunitsList = cunitsList;
 	}
-
-
-
-	public Vector<Method> getMethodsList() {
+	
+	public Vector<Method> getMethodsList()
+	{
 		return methodsList;
 	}
-
-
-
-	public Vector<Class> getClassesList() {
-		return classesList;
-	}
-
-
 
 	public void setMethodsList(Vector<Method> methodsList) 
 	{
 		this.methodsList = methodsList;
 	}
-
-
-
-	public void setClassesList(Vector<Class> classesList) {
+	
+	public Vector<Class> getClassesList()
+	{
+		return classesList;
+	}
+	
+	public void setClassesList(Vector<Class> classesList)
+	{
 		this.classesList = classesList;
 	}
 
-	private Stack stStack, methodsStack, classesStack, cuStack;
-
-	public AstExplorerVisitor() 
+	public Vector<Statement> getStatementsList()
 	{
-		super();
-		//	cuMetadata = new CuMetadata(new Vector<String>());
-		stStack = new Stack<ASTNode>();
-		methodsStack = new Stack<ASTNode>();
-		classesStack = new Stack<ASTNode>();
-		cuStack = new Stack<ASTNode>();
-		
-		statementsList =  new Vector<Statement>();
-		methodsList = new Vector<Method>();
-		classesList = new Vector<Class>();
-		cunitsList = new Vector<CompilationUnit>();
-	}
-
-
-
-	public Vector<Statement> getStatementsList() {
 		return statementsList;
 	}
 
-
-
-	public void setStatementsList(Vector<Statement> statementsList) {
+	public void setStatementsList(Vector<Statement> statementsList)
+	{
 		this.statementsList = statementsList;
 	}
+	
+	public Stack getStStack()
+	{
+		return stStack;
+	}
 
+	public Stack getMethodsStack()
+	{
+		return methodsStack;
+	}
 
+	public Stack getClassesStack()
+	{
+		return classesStack;
+	}
+
+	public Stack getCuStack()
+	{
+		return cuStack;
+	}
+
+	public void setStStack(Stack stStack)
+	{
+		this.stStack = stStack;
+	}
+
+	public void setMethodsStack(Stack methodsStack)
+	{
+		this.methodsStack = methodsStack;
+	}
+
+	public void setClassesStack(Stack classesStack)
+	{
+		this.classesStack = classesStack;
+	}
+
+	public void setCuStack(Stack cuStack)
+	{
+		this.cuStack = cuStack;
+	}
+
+	/**
+	 * AST pre-visitor
+	 * Push symbols into their corresponding stack to prepare them for processing
+	 */
 	@Override
 	public void preVisit(ASTNode node) {
 
 		super.preVisit(node);
-		/*if (node instanceof org.eclipse.jdt.core.dom.Statement){
-			stStack.push(node);	
-		}*/
 
 		if(node instanceof Expression)
 		{
 			stStack.push(node);
 		}
 
-		switch (node.getNodeType()){
-
-		case ASTNode.TYPE_DECLARATION : classesStack.push(node); break;
-
-		case ASTNode.METHOD_DECLARATION : methodsStack.push(node); break;
-
-		case ASTNode.COMPILATION_UNIT : cuStack.push(node); break;
-
-		/*case ASTNode.ARRAY_INITIALIZER :
-		case ASTNode.ASSIGNMENT : 
-		case ASTNode.CONDITIONAL_EXPRESSION :
-		case ASTNode.INFIX_EXPRESSION :
-		case ASTNode.INITIALIZER : 
-		case ASTNode.POSTFIX_EXPRESSION:
-		case ASTNode.PREFIX_EXPRESSION:
-		case ASTNode.VARIABLE_DECLARATION_STATEMENT:	
-			stStack.push(node); break;*/
+		switch (node.getNodeType())
+		{
+			case ASTNode.TYPE_DECLARATION : classesStack.push(node); break;
+			case ASTNode.METHOD_DECLARATION : methodsStack.push(node); break;
+			case ASTNode.COMPILATION_UNIT : cuStack.push(node); break;
 		
-
-		default: break;
+			default: break;
 		}
 		
-		if(node.getNodeType() == ASTNode.BLOCK){
+		if(node.getNodeType() == ASTNode.BLOCK)
+		{
 			switch(node.getParent().getNodeType())
 			{
-			default: break;
-
-			case ASTNode.WHILE_STATEMENT : 
-			case ASTNode.FOR_STATEMENT : 
-			case ASTNode.ENHANCED_FOR_STATEMENT : 
-			case ASTNode.IF_STATEMENT: 
-				nblevelsCount++;
-				break;				
-
+				default: break;
+	
+				case ASTNode.WHILE_STATEMENT : 
+				case ASTNode.FOR_STATEMENT : 
+				case ASTNode.ENHANCED_FOR_STATEMENT : 
+				case ASTNode.IF_STATEMENT: 
+					nblevelsCount++;
+					break;				
 			}
-
 		}
 	}
 
+	/**
+	 * AST visiting routines
+	 * Cut-off by returning false to avoid going deeper down the AST
+	 */ 
 	@Override
-	public boolean visit(Assignment node) {
-
-		return false;
-	}
-
-
-
-	@Override
-	public boolean visit(ArrayAccess node) {
-	
+	public boolean visit(Assignment node)
+	{
 		return false;
 	}
 
 	@Override
-	public boolean visit(TypeDeclaration node) {
-		
+	public boolean visit(ArrayAccess node)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean visit(TypeDeclaration node)
+	{
 		FieldDeclaration[] classFields =  new FieldDeclaration[node.getFields().length];
 		System.out.println(node.getFields().length);
-	    classFields= node.getFields();
+	    	classFields= node.getFields();
 	    
-	    for (FieldDeclaration classField :classFields){
-	    	
-	    	int modifiers = classField.getModifiers();
-	    	
-	    	System.out.println(classField.toString());
-	    	
-	    	if (Modifier.isPublic(modifiers)){
-	    		
-	    		nbOfPublicMembers++;
-	    		
-	    		System.out.println("public field detected ");
-	    		
+	    	for (FieldDeclaration classField :classFields)
+	    	{
+	    		int modifiers = classField.getModifiers();
+	    		System.out.println(classField.toString());
+	   
+	    		if (Modifier.isPublic(modifiers))
+	    		{
+	    			nbOfPublicMembers++;
+	    			System.out.println("public field detected ");
+	    		}
 	    	}
-	    }
-		
 		return super.visit(node);
 	}
 
-
+	@Override
+	public boolean visit(ArrayCreation node)
+	{	
+		return false;
+	}
 
 	@Override
-	public boolean visit(ArrayCreation node) {
+	public boolean visit(ArrayInitializer node)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean visit(ClassInstanceCreation node)
+	{
 		
 		return false;
 	}
 
 	@Override
-	public boolean visit(ArrayInitializer node) {
-	
+	public boolean visit(ConditionalExpression node)
+	{	
 		return false;
 	}
 
 	@Override
-	public boolean visit(ClassInstanceCreation node) {
-		
+	public boolean visit(FieldAccess node)
+	{
 		return false;
 	}
 
 	@Override
-	public boolean visit(ConditionalExpression node) {
-		
+	public boolean visit(InfixExpression node) 
+	{	
 		return false;
 	}
 
 	@Override
-	public boolean visit(FieldAccess node) {
-		
+	public boolean visit(ParenthesizedExpression node)
+	{	
 		return false;
 	}
 
 	@Override
-	public boolean visit(InfixExpression node) {
-		
+	public boolean visit(PostfixExpression node)
+	{	
 		return false;
 	}
 
 	@Override
-	public boolean visit(ParenthesizedExpression node) {
-		
+	public boolean visit(PrefixExpression node)
+	{	
 		return false;
 	}
 
 	@Override
-	public boolean visit(PostfixExpression node) {
-		
-		return false;
-	}
-
-	@Override
-	public boolean visit(PrefixExpression node) {
-		
-		return false;
-	}
-
-	@Override
-	public boolean visit(VariableDeclarationExpression node) {
-		
+	public boolean visit(VariableDeclarationExpression node)
+	{	
 		return false;
 	}
 	
@@ -271,45 +293,30 @@ public class AstExplorerVisitor extends ASTVisitor {
 		return false;
 	}
 	
-	
-
+	/**
+	 * AST post-visitor
+	 * Pop symbols off and add them to their corresponding vectors
+	 */
 	@Override
-	public void postVisit(ASTNode node) {
-		
-		
+	public void postVisit(ASTNode node)
+	{	
 		super.postVisit(node);
 		
-		if(node.getNodeType() == ASTNode.BLOCK){
+		if(node.getNodeType() == ASTNode.BLOCK)
+		{
 			switch(node.getParent().getNodeType())
 			{
-			default: break;
-
-			case ASTNode.WHILE_STATEMENT : 
-			case ASTNode.FOR_STATEMENT : 
-			case ASTNode.ENHANCED_FOR_STATEMENT : 
-			case ASTNode.IF_STATEMENT: 
-				nblevelsCount--;
-				break;				
-
+				case ASTNode.WHILE_STATEMENT : 
+				case ASTNode.FOR_STATEMENT : 
+				case ASTNode.ENHANCED_FOR_STATEMENT : 
+				case ASTNode.IF_STATEMENT: 
+					nblevelsCount--;
+					break;	
+					
+				default: break;
 			}
-
 		}
-	
-
-		/*switch(node.getNodeType())
-		{
-			case ASTNode.ARRAY_INITIALIZER :
-			case ASTNode.ASSIGNMENT : 
-			//case ASTNode.CONDITIONAL_EXPRESSION :
-			case ASTNode.INFIX_EXPRESSION :
-			case ASTNode.INITIALIZER : 
-			case ASTNode.POSTFIX_EXPRESSION:
-			case ASTNode.PREFIX_EXPRESSION:
-			case ASTNode.VARIABLE_DECLARATION_STATEMENT:
-				Statement st = new Statement(node.toString());
-				st.setNumberOfOperators(new IntegerMetric("numberOfOperators", calculateNumberOfOperators(node)));
-				statementsList.add(st);
-		}*/
+		
 		if(node instanceof Expression)
 		{
 			Statement st = new Statement(node.toString());
@@ -320,27 +327,27 @@ public class AstExplorerVisitor extends ASTVisitor {
 			/* Set method metrics in which this stmt exists */
 		}
 		
-		if (node instanceof TypeDeclaration){
-			
-           Class clazz = new Class(node.toString());
-           
-           clazz.setNumberOfPublicMembers(new IntegerMetric ("numberOfPublicMembers",nbOfPublicMembers));
-           
-           classesList.add(clazz);
-           
-           classesStack.pop();
-			
+		if (node instanceof TypeDeclaration)
+		{		
+	           Class clazz = new Class(node.toString());
+	           
+	           clazz.setNumberOfPublicMembers(new IntegerMetric ("numberOfPublicMembers",nbOfPublicMembers));
+	           classesList.add(clazz);
+	           classesStack.pop();		
 		}
 		
 	}
 
-	Integer calculateNumberOfOperators(ASTNode stmt)
+	/**
+	 * Helper method for calculating the number of operators in a the given statement node
+	 * Works by recursively processing nodes in the subtree for this statement
+	 */
+	private Integer calculateNumberOfOperators(ASTNode stmt)
 	{
 		Integer opcount = 0;
+		
 		switch(stmt.getNodeType())
 		{
-			default: break;
-			
 			case ASTNode.ARRAY_ACCESS:
 				ArrayAccess arrAccess = (ArrayAccess) stmt;
 				opcount = calculateNumberOfOperators(arrAccess.getArray()) +
@@ -361,10 +368,12 @@ public class AstExplorerVisitor extends ASTVisitor {
 				opcount = 1 + calculateNumberOfOperators(inExpr.getLeftOperand())
 						+ calculateNumberOfOperators(inExpr.getRightOperand());
 				break;
+				
 			case ASTNode.POSTFIX_EXPRESSION:
 				PostfixExpression postExpr = (PostfixExpression) stmt;
 				opcount = 1 + calculateNumberOfOperators(postExpr.getOperand());
 				break;
+				
 			case ASTNode.PREFIX_EXPRESSION:
 				PrefixExpression preExpr = (PrefixExpression) stmt;
 				opcount = 1 + calculateNumberOfOperators(preExpr.getOperand());
@@ -375,15 +384,6 @@ public class AstExplorerVisitor extends ASTVisitor {
 				opcount = 1 + calculateNumberOfOperators(rhs.getLeftHandSide()) +
 						calculateNumberOfOperators(rhs.getRightHandSide());
 				break;
-				
-			/*case ASTNode.INITIALIZER:
-				Initializer init = (Initializer) stmt;
-				List<org.eclipse.jdt.core.dom.Statement> sts = init.getBody().statements();
-				for(org.eclipse.jdt.core.dom.Statement s: sts)
-				{
-					opcount += calculateNumberOfOperators(s);
-				}
-				break;*/
 				
 			case ASTNode.VARIABLE_DECLARATION_STATEMENT:
 				VariableDeclarationStatement vds = (VariableDeclarationStatement) stmt;
@@ -400,65 +400,10 @@ public class AstExplorerVisitor extends ASTVisitor {
 				if (exp != null) opcount = 1 + calculateNumberOfOperators(exp);
 				
 				break;
+				
+			default: break;
 		}
+		
 		return opcount;
 	}
-	
-	/*Integer calculateNumberOfLevels(ASTNode stmt){
-		
-		Integer nblcount = 0;
-		
-		switch(stmt.getNodeType())
-		{
-			default: break;
-			 
-			case ASTNode.WHILE_STATEMENT : 
-				
-				WhileStatement ws = (WhileStatement) stmt;
-				
-				
-				break;
-			case ASTNode.FOR_STATEMENT : break;
-			case ASTNode.ENHANCED_FOR_STATEMENT : break;
-			case ASTNode.IF_STATEMENT: break;				
-			
-		}
-		
-		return nblcount;
-	}*/
-
-	public Stack getStStack() {
-		return stStack;
-	}
-
-	public Stack getMethodsStack() {
-		return methodsStack;
-	}
-
-	public Stack getClassesStack() {
-		return classesStack;
-	}
-
-	public Stack getCuStack() {
-		return cuStack;
-	}
-
-	public void setStStack(Stack stStack) {
-		this.stStack = stStack;
-	}
-
-	public void setMethodsStack(Stack methodsStack) {
-		this.methodsStack = methodsStack;
-	}
-
-	public void setClassesStack(Stack classesStack) {
-		this.classesStack = classesStack;
-	}
-
-	public void setCuStack(Stack cuStack) {
-		this.cuStack = cuStack;
-	}
-
-
-
 }
